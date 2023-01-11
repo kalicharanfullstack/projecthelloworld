17eff771b3c6b7f89e93835b0cb7f4b94e17a57f
kalicharanfullstack <kalicharanfullstack@gmail.com>
2023-01-10 23:08:45 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import HelloWorld from "./component/HelloWorld";
+
+function App() {
+  return <HelloWorld></HelloWorld>;
+}
+
+export default App;
---
+++ src/component/HelloWorld.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import { Component } from "react";
+
+export default class HelloWorld extends Component{
+
+    render(){
+
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
